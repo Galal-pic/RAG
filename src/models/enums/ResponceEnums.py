309e0d4c883c25111bcd9a908121bd7fb8763d5b
Galal Ewida <galalamrewida@gmail.com>
2024-09-21 21:40:28 +0300

    create uplad

---
+++ src/models/enums/ResponceEnums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ResponceSignal(Enum):
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_VALIDATE_NOT_SUPPORTED = "file_validate_not_supported"
+    FILE_SIZE_LIMIT_EXCEEDED = "file_size_limit_exceeded"
+    FILE_UPLOAED_SUCCESS = "file_aploaed_success"
+    FILE_UPLOAED_FAILED = "file_upload_failed"
